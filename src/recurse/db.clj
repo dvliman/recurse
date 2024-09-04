4bbaf59746ad3abc8c67ae10a714c04382267e18
David Liman <limanoit@gmail.com>
2024-09-04 13:42:43 -0700

    compact and swap file reference

--- src/recurse/db.clj
+++ src/recurse/db.clj
@@ -5,15 +5,24 @@
    [clojure.java.io :as io]))
 
 (defmethod ig/init-key ::db [_ {:keys [filepath] :as opts}]
-  {:filepath filepath
-   :data (atom (into {}
-                 (comp (partition-all 2)
-                   (map (fn [[k v]]
-                          [(edn/read-string k)
-                           (edn/read-string v)])))
-                 (try
-                   (line-seq (io/reader filepath))
-                   (catch Exception _ []))))})
+  (let [data (into {}
+                   (comp (partition-all 2)
+                         (map (fn [[k v]]
+                                [(edn/read-string k)
+                                 (edn/read-string v)])))
+                   (try
+                     (line-seq (io/reader filepath))
+                     (catch Exception _ [])))
+        backup-filepath    (str filepath ".backup")
+        compacted-filepath (str filepath ".compact")]
+    (with-open [out (io/writer compacted-filepath)]
+      (doseq [[k v] data]
+        (.write out (prn-str k))
+        (.write out (prn-str v))))
+    (.renameTo (io/file filepath) (io/file backup-filepath))
+    (.renameTo (io/file compacted-filepath) (io/file filepath))
+    {:filepath filepath
+     :data (atom data)}))
 
 (defmethod ig/halt-key! ::db [_ _]
   {})
